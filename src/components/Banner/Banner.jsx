48a458944193d843c9357e8d6509c75e10c5f5fa
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2022-06-23 18:15:36 +0600

    banner updated

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,35 +1,31 @@
-import { CCarousel, CCarouselCaption, CCarouselItem, CImage } from '@coreui/react';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
 import React from 'react';
-import banner1 from '../../assets/01.jpg';
+import OwlCarousel from 'react-owl-carousel';
 import banner2 from '../../assets/02.jpg';
 import banner3 from '../../assets/03.jpg';
 import './Banner.css';
 
+
 const Banner = () => {
+
+
   return (
-    <CCarousel controls indicators>
-      <CCarouselItem>
-        <CImage className="d-block w-100 banner" src={banner1} alt="slide 1" />
-        <CCarouselCaption className="d-none d-md-block">
-          <h3 class="animate-charcter"> Pro Health Care</h3>
-          <p className='animate-p'>We are always here for you</p>
-        </CCarouselCaption>
-      </CCarouselItem>
-      <CCarouselItem>
-        <CImage className="d-block w-100 banner" src={banner2} alt="slide 2" />
-        <CCarouselCaption className="d-none d-md-block">
-          <h3 class="animate-charcter"> Pro Health Care</h3>
-          <p className='animate-p'>We are always here for you</p>
-        </CCarouselCaption>
-      </CCarouselItem>
-      <CCarouselItem>
-        <CImage className="d-block w-100 banner" src={banner3} alt="slide 3" />
-        <CCarouselCaption className="d-none d-md-block">
-          <h3 class="animate-charcter"> Pro Health Care</h3>
-          <p className='animate-p'>We are always here for you</p>
-        </CCarouselCaption>
-      </CCarouselItem>
-    </CCarousel>
+    <div class='container-fluid position-relative' >
+      <OwlCarousel items={1}
+        className="owl-theme"
+        loop
+        nav
+        autoplay
+        margin={0}
+      >
+        <div ><img className="img" src={banner3} /></div>
+        <div><img className="img" src={banner2} /></div>
+        <div><img className="img" src={banner3} /></div>
+
+      </OwlCarousel>
+
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
